112f58c507b134cbc5c1e763415829ac6dc749bd
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-25 14:35:45 -0300

    Add devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Nunito_Sans } from "@next/font/google";
 
 const nunitoSans = Nunito_Sans({
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className={`${nunitoSans.className}`}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </div>
   );
