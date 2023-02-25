fd84a57fe8f082f20ee7eafbfa43ce11d5f5ef79
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-25 11:05:03 -0300

    Install tanstack query Added query client provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import {Nunito_Sans} from "@next/font/google"
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Nunito_Sans } from "@next/font/google";
 
-const nunitoSans = Nunito_Sans(
-  {
-    subsets:["latin"],
-    weight: ["300", "600", "800"],
-    variable: "--ff-sans"
-  }
-)
+const nunitoSans = Nunito_Sans({
+  subsets: ["latin"],
+  weight: ["300", "600", "800"],
+  variable: "--ff-sans",
+});
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className={`${nunitoSans.className}`}>
-    <Component {...pageProps} />
-  </div>
+  return (
+    <div className={`${nunitoSans.className}`}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </div>
+  );
 }
