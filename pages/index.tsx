5f8bd0cc199857601c397641f54e486b06b214a2
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-26 17:31:57 -0300

    Fetch and render countries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,68 @@
-import { NextPage } from "next"
+import Layout from "@/components/Layout";
+import { useGetAllCountries } from "@/hooks/countries";
+import { NextPage } from "next";
+import Image from "next/image";
 const Home: NextPage = () => {
-    return <div>Home</div>
-}
-export default Home
+  const { data: countries, isLoading, isError } = useGetAllCountries();
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error...</p>;
+  return (
+    <Layout title="What in the world | Home">
+      <form className="">
+        <input type="text" placeholder="Search for a country" />
+        <select name="region" id="region">
+          <option>Filter by Region</option>
+          <option value="africa">Africa</option>
+          <option value="america">America</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+      </form>
+      <div>
+        {countries.map((country) => {
+          const {
+            name,
+            flags,
+            population,
+            capital: capitals,
+            region,
+          } = country;
+          const alt = flags.png && `The ${name.common} flag`;
+
+          return (
+            <div key={name.common}>
+              <div className="relative h-32">
+                <Image src={flags.png} alt={alt} fill />
+              </div>
+              <div>
+                <h3>{name.common}</h3>
+                <ul>
+                  <li>
+                    <p>
+                      <b>Population: </b>
+                      {population}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>capital: </b>
+                      {capitals ? capitals.join(",") : "Not found"}
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>region: </b>
+                      {region}
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
+  );
+};
+export default Home;
