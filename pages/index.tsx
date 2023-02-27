506a97ffe7bec1061c904c7ed822d2448e73cf43
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-27 17:31:19 -0300

    render dynamically all countries or by region

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import CountriesList from "@/components/CountriesList";
+import Countries from "@/components/Countries";
+import CountriesByRegion from "@/components/CountriesByRegion";
 import Filter from "@/components/Filter";
 import Layout from "@/components/Layout";
+import { filterStore } from "@/store/filter";
 import { NextPage } from "next";
 const Home: NextPage = () => {
+  const region = filterStore((state) => state.region);
   return (
     <Layout title="What in the world | Home">
       <Filter />
-      <CountriesList />
+      {region !== "" ? <CountriesByRegion region={region} /> : <Countries />}
     </Layout>
   );
 };
