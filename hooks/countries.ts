bcb988aacda4d16fe45f3fbb4b5c15cfd49b2d1d
Elian Avalos <elianemanuelstudy@gmail.com>
2023-02-28 14:33:10 -0300

    Add get country hook. Modified validator to accept more values.

--- hooks/countries.ts
+++ hooks/countries.ts
@@ -1,4 +1,8 @@
-import { getAllCountries, getCountriesByRegion } from "@/api/countries";
+import {
+  getAllCountries,
+  getCountriesByRegion,
+  getCountry,
+} from "@/api/countries";
 import { useQuery } from "@tanstack/react-query";
 import { z } from "zod";
 
@@ -6,6 +10,15 @@ const countryParser = z.object({
   name: z.object({
     common: z.string(),
     official: z.string(),
+    nativeName: z
+      .object({})
+      .catchall(
+        z.object({
+          common: z.string(),
+          official: z.string(),
+        })
+      )
+      .optional(),
   }),
   flags: z.object({
     png: z.string().url(),
@@ -16,6 +29,18 @@ const countryParser = z.object({
   region: z.string(),
   subregion: z.string().optional(),
   capital: z.string().array().optional(),
+  tld: z.string().array().optional(),
+  currencies: z
+    .object({})
+    .catchall(
+      z.object({
+        name: z.string(),
+        symbol: z.string().optional(),
+      })
+    )
+    .optional(),
+  languages: z.object({}).catchall(z.string()).optional(),
+  borders: z.string().array().optional(),
 });
 
 const countriesParser = z.array(countryParser);
@@ -33,5 +58,12 @@ export const useGetCountriesByRegion = (region: string) =>
   useQuery(["countries", region], async () => {
     const res = await getCountriesByRegion(region);
     const parsedRes = countriesParser.parse(res.data);
+    return parsedRes;
+  });
+
+export const useGetCountry = (name: string) =>
+  useQuery(["country", name], async () => {
+    const res = await getCountry(name);
+    const parsedRes = countryParser.parse(res.data[0]);
     return parsedRes;
   });
